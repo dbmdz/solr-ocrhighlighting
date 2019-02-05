7199de0921e2815a5abb1a2c944d920c32d7e094
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2019-02-05 11:07:54 +0100

    Massive optimizations for FileCharIter
    By leveraging a MappedByteBuffer, I/O expenses become almost negligible.

--- src/test/java/org/mdz/search/solrocr/util/FileCharIteratorTest.java
+++ src/test/java/org/mdz/search/solrocr/util/FileCharIteratorTest.java
@@ -1,10 +1,11 @@
 package org.mdz.search.solrocr.util;
 
+import com.google.common.math.IntMath;
 import java.io.IOException;
-import java.net.URISyntaxException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.text.CharacterIterator;
 import java.util.Random;
 import org.assertj.core.internal.bytebuddy.utility.RandomString;
 import org.junit.jupiter.api.AfterEach;
@@ -13,15 +14,18 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-class FileCharIterableTest {
-  String testText;
-  Path testFile;
+class FileCharIteratorTest {
+  private final static int TEXT_SIZE = IntMath.pow(2, 18); // 256k chars, i.e. a 512KiB file
+  private String testText;
+  private Path testFile;
+  private FileCharIterator it;
 
   @BeforeEach
   void setUp() throws IOException {
-    this.testText = RandomString.make(2^16);
+    this.testText = RandomString.make(TEXT_SIZE);
     this.testFile = Files.createTempFile("solrocrtest", ".txt");
     Files.write(this.testFile, this.testText.getBytes(StandardCharsets.UTF_16));
+    this.it = new FileCharIterator(testFile);
   }
 
   @AfterEach
@@ -30,16 +34,14 @@ class FileCharIterableTest {
   }
 
   @Test
-  void testInit() throws IOException {
-    FileCharIterable it = new FileCharIterable(testFile);
-    assertThat(it.length()).isEqualTo(2^16);
+  void testInit() {
+    assertThat(it.length()).isEqualTo(TEXT_SIZE);
   }
 
   @Test
-  void testSeek() throws IOException, URISyntaxException {
-    FileCharIterable it = new FileCharIterable(testFile);
+  void testSeek() {
     Random rand = new Random();
-    for (int i=0; i < 100000; i++) {
+    for (int i = 0; i < TEXT_SIZE / 4; i++) {
       int idx = rand.nextInt(testText.length() - 6);
       char decoded = it.charAt(idx);
       char fromString = testText.charAt(idx);
@@ -47,4 +49,16 @@ class FileCharIterableTest {
     }
   }
 
+  @Test
+  void testSubSequence() {
+    String strSeq = testText.substring(10, 100);
+    String itSeq = it.subSequence(10, 100).toString();
+    assertThat(itSeq).isEqualTo(strSeq);
+  }
+
+  @Test
+  void testSeekEnd() throws IOException {
+    it.setIndex(it.getEndIndex());
+    assertThat(it.current()).isEqualTo(CharacterIterator.DONE);
+  }
 }
