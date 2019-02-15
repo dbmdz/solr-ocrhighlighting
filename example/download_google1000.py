4ee443a289eba9fcf3e1e51f6c1c1a84fcdaefba
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2019-02-15 19:09:27 +0100

    Add example docker setup with Google1000 dataset

---
+++ example/download_google1000.py
@@ -0,0 +1,49 @@
+import sys
+from concurrent.futures import ProcessPoolExecutor, as_completed
+from io import BytesIO
+from pathlib import Path
+from zipfile import ZipFile
+
+import lxml.etree as etree
+import requests
+
+URL_TEMPLATE = "http://commondatastorage.googleapis.com/books/icdar2007/Volume_{:04}.zip"
+
+parser = etree.HTMLParser()
+
+
+def download_volume(vol_num: int, out_dir: Path):
+    vol_dir = out_dir / f'Volume_{vol_num:04}'
+    if not vol_dir.exists():
+        resp = requests.get(URL_TEMPLATE.format(vol_num))
+        zf = ZipFile(BytesIO(resp.content))
+        zf.extractall(str(out_dir))
+    fix_hocr(vol_dir / 'hOCR.html')
+    return vol_dir
+
+
+def fix_hocr(hocr_path: Path):
+    tree = etree.parse(str(hocr_path), parser=parser)
+    for idx, page_elem in enumerate(tree.findall('.//div[@class="ocr_page"]'),
+                                    start=1):
+        page_elem.attrib['id'] = f'page_{idx}'
+        for word_elem in page_elem.findall('.//span[@class="ocr_cinfo"]'):
+            word_elem.attrib['class'] = 'ocrx_word'
+    with hocr_path.open('wb') as fp:
+        fp.write(etree.tostring(tree))
+
+
+def main():
+    out_dir = Path('./google1000')
+    out_dir.mkdir(exist_ok=True)
+    with ProcessPoolExecutor(max_workers=8) as pool:
+        futs = []
+        for vol_num in range(1000):
+            futs.append(pool.submit(download_volume, vol_num, out_dir))
+        for idx, fut in enumerate(as_completed(futs)):
+            sys.stdout.write(f'{idx + 1}/1000\r')
+            sys.stdout.flush()
+
+
+if __name__ == '__main__':
+    main()
---
+++ example/ingest_google1000.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+import re
+import sys
+import tarfile
+from pathlib import Path
+
+import requests
+
+
+GOOGLE1000_PATH = Path('./google1000')
+SOLR_HOST = 'localhost:8983'
+SOLR_CORE = 'ocrtest'
+
+
+class SolrException(Exception):
+    def __init__(self, resp, payload):
+        self.message = resp
+        self.payload = payload
+
+
+def index_documents(docs):
+    resp = requests.post(
+        "http://{}/solr/{}/update".format(SOLR_HOST, SOLR_CORE),
+        json=docs, params=dict(softCommit="true"))
+    if not resp:
+        raise SolrException(resp.json(), docs)
+
+
+def load_ocrtext(base_dir):
+    base_dir = Path(base_dir)
+    for vol_dir in base_dir.glob('Volume_*'):
+        hocr_path = vol_dir / 'hOCR.html'
+        if not hocr_path.exists():
+            continue
+        with hocr_path.open("rt") as fp:
+            yield vol_dir.name, fp.read()
+
+
+if __name__ == '__main__':
+    ocr_iter = load_ocrtext(GOOGLE1000_PATH)
+    batch = []
+    for ident, ocr in ocr_iter:
+        doc = dict(id=ident.split("_")[-1], vol_name=ident, ocr_text=ocr)
+        index_documents([doc])
