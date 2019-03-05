004f74b216c780669cfbb916627619701da19eff
Johannes Baiter <johannes.baiter@gmail.com>
2019-03-05 11:17:01 +0100

    Add support for indexing escaped ASCII ALTO content.
    Works by providing a custom CharFilter that strips some non-content text
    and makes sure that the `HTMLStripCharFilter` does not swallow the OCR
    text inside of the `String/@CONTENT` attributes.

---
+++ src/main/java/org/mdz/search/solrocr/formats/alto/AltoCharFilterFactory.java
@@ -0,0 +1,41 @@
+package org.mdz.search.solrocr.formats.alto;
+
+import java.io.Reader;
+import java.util.Map;
+import java.util.regex.Pattern;
+import org.apache.lucene.analysis.CharFilter;
+import org.apache.lucene.analysis.pattern.PatternReplaceCharFilter;
+import org.apache.lucene.analysis.util.AbstractAnalysisFactory;
+import org.apache.lucene.analysis.util.CharFilterFactory;
+import org.apache.lucene.analysis.util.MultiTermAwareComponent;
+
+/**
+ * CharFilter to prepare ALTO input for consumption by a
+ * {@link org.apache.lucene.analysis.charfilter.HTMLStripCharFilter}.
+ *
+ * This filter will:
+ * <ul>
+ *   <li>Strip out the <code>&lt;Description&gt;...&lt;/Description&gt;</code> element</li>
+ *   <li>Extract the OCRed text out of the <code>CONTENT</code> attribute into a text node so it's not removed by
+ *   the {@link org.apache.lucene.analysis.charfilter.HTMLStripCharFilter}</li>
+ * </ul>
+ */
+public class AltoCharFilterFactory extends CharFilterFactory implements MultiTermAwareComponent {
+  private static final Pattern DESC_PAT = Pattern.compile("<Description>.+</Description", Pattern.DOTALL);
+  private static final Pattern CONTENT_PAT = Pattern.compile("CONTENT=['\"](.+?)['\"]");
+
+  public AltoCharFilterFactory(Map<String, String> args) {
+    super(args);
+  }
+
+  @Override
+  public Reader create(Reader input) {
+    CharFilter descFilter = new PatternReplaceCharFilter(DESC_PAT, "", input);
+    return new PatternReplaceCharFilter(CONTENT_PAT, "        >$1<", descFilter);
+  }
+
+  @Override
+  public AbstractAnalysisFactory getMultiTermComponent() {
+    return this;
+  }
+}
