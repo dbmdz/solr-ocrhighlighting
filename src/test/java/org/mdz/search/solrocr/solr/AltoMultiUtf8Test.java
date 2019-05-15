1e317dd78e0c4681358ea40392b4e018e8e9ac22
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2019-05-15 13:21:26 +0200

    Fix IndexOutOfBunds error when adjust final offset

--- src/test/java/org/mdz/search/solrocr/solr/AltoMultiUtf8Test.java
+++ src/test/java/org/mdz/search/solrocr/solr/AltoMultiUtf8Test.java
@@ -96,6 +96,6 @@ public class AltoMultiUtf8Test extends SolrTestCaseJ4 {
         "count(//arr[@name='snippets']/lst)=1",
         "(//arr[@name='regions']/lst/str[@name='page'])[1]/text()='P2'",
         "(//arr[@name='regions']/lst/str[@name='page'])[2]/text()='P3'",
-        "//arr[@name='snippets']/lst/str[@name='text']/text()='Vcschllisse motivirt hat. wird gcwisi nirgends mchr gcwurdigt und tiaukbarer anerkannt, alS hier in unscrcm Landc; abcr auch <em>nirgendS H bas Bediirfnisi</em> nach ciucr cndlichen That des Bundcs dringender alS hier. Die bishcrige'");
+        "//arr[@name='snippets']/lst/str[@name='text']/text()='Vcschllisse motivirt hat. wird gcwisi nirgends mchr gcwurdigt und tiaukbarer anerkannt, alS hier in unscrcm Landc; abcr auch <em>nirgendS</em> H bas <em>Bediirfnisi</em> nach ciucr cndlichen That des Bundcs dringender alS hier. Die bishcrige'");
   }
 }
