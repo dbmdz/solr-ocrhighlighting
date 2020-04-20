2877e4cea514392dcdf6d622c23137f5fbcf58fa
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2020-04-20 17:24:21 +0200

    example: Fix plugin installation script
    The script required Pythton, which is gone from the Docker image from
    version 8.5 on. We instead install `jq` now and use that.
    Additionally, the script now checks if there's locally built version of
    the plugin available and uses that instead of the GitHub release
    version.

--- example/solr/install-plugin.sh
+++ example/solr/install-plugin.sh
@@ -1,20 +1,25 @@
 #!/bin/bash
 set -e
 
-function extract_json() {
-    path="$1"
-    python -c "import json, sys; print json.load(sys.stdin)$path"
-}
-
 RELEASE_URL="https://api.github.com/repos/dbmdz/solr-ocrhighlighting/releases/latest"
 PLUGIN_LIB_DIR="/var/solr/data/plugins"
 
+wget -q https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64 -O/tmp/jq
+chmod +x /tmp/jq
+
 mkdir -p "$PLUGIN_LIB_DIR"
 
-# Get the URL and filename for the latest release
-release_desc="$(curl -sS "$RELEASE_URL")"
-fname="$(echo "$release_desc" |extract_json "['assets'][0]['name']")"
-if [ ! -f "$PLUGIN_LIB_DIR"/"$fname" ]; then
-    url="$(echo "$release_desc" |extract_json "['assets'][0]['browser_download_url']")"
-    wget "$url" -P "$PLUGIN_LIB_DIR"
+if [ "$(ls /build |grep 'jar')" ]; then
+    # Copy the plugin from the build directory
+    echo "Installing plugin from build directory"
+    cp /build/solr-ocrhighlighting-*.jar "$PLUGIN_LIB_DIR"/
+else
+    # Get the URL and filename for the latest release
+    echo "Installing plugin from latest GitHub release"
+    release_desc="$(curl -sS "$RELEASE_URL")"
+    fname="$(echo "$release_desc" |/tmp/jq -r ".assets[0].name")"
+    if [ ! -f "$PLUGIN_LIB_DIR"/"$fname" ]; then
+        url="$(echo "$release_desc" |/tmp/jq -r ".assets[0].browser_download_url")"
+        wget -q "$url" -P "$PLUGIN_LIB_DIR"
+    fi
 fi
