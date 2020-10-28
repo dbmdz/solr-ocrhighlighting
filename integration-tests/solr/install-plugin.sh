9b341faaa312bafd6047c0c610bd492cf3bad919
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2020-10-28 18:41:35 +0100

    Fix inclusion of aalto-xml in fat jar

--- integration-tests/solr/install-plugin.sh
+++ integration-tests/solr/install-plugin.sh
@@ -5,4 +5,4 @@ PLUGIN_LIB_DIR="/var/solr/data/plugins"
 BUILD_PATH="/build"
 
 mkdir -p "$PLUGIN_LIB_DIR"
-cp "$BUILD_PATH"/*.jar "$PLUGIN_LIB_DIR"
+cp "$BUILD_PATH"/solr-ocrhighlighting*.jar "$PLUGIN_LIB_DIR"
