ea52cd45577c1921112f9f4804600840bdf76df2
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2020-10-29 10:11:27 +0100

    Optimize attribute offset detection

--- src/main/java/de/digitalcollections/solrocr/formats/alto/AltoStaxFilterFactory.java
+++ src/main/java/de/digitalcollections/solrocr/formats/alto/AltoStaxFilterFactory.java
@@ -5,9 +5,7 @@ import com.ctc.wstx.exc.WstxEOFException;
 import com.ctc.wstx.stax.WstxInputFactory;
 import java.io.IOException;
 import java.io.Reader;
-import java.io.StringWriter;
 import java.util.HashMap;
-import javax.xml.namespace.QName;
 import javax.xml.stream.XMLStreamConstants;
 import javax.xml.stream.XMLStreamException;
 import org.apache.lucene.analysis.charfilter.BaseCharFilter;
@@ -68,11 +66,8 @@ public class AltoStaxFilterFactory extends CharFilterFactory {
         if (subsTypeIdx >= 0) {
           contentAttrib = "SUBS_CONTENT";
         }
-        long inputOffset = xmlReader.getLocationInfo().getStartingCharOffset();
-        inputOffset += (
-            getSelfClosingElementString().indexOf(contentAttrib)
-                + contentAttrib.length() + 2);
-        int cumulativeDiff = (int) (inputOffset - outputOffset);
+        long contentOffset = getAttributeValueOffset(contentAttrib);
+        int cumulativeDiff = (int) (contentOffset - outputOffset);
         this.addOffCorrectMap(outputOffset, cumulativeDiff);
         int contentIdx = xmlReader.getAttributeIndex("", contentAttrib);
         String txt = xmlReader.getAttributeValue(contentIdx);
@@ -117,48 +112,49 @@ public class AltoStaxFilterFactory extends CharFilterFactory {
       return numRead;
     }
 
-    /** Get the XML string representation of the current element.
+    /** Get the character offset of the value for the given attribute in the reader.
      *
      * Assumes the XMLStreamReader is on a START_ELEMENT event.
-     * Assumes the XML is using minimal whitespace and is situated on a single line.
+     * Assumes the XML element is using minimal whitespace and is situated on a single line.
      */
-    private String getSelfClosingElementString() {
-      StringBuilder sb = new StringBuilder();
-      sb.append('<');
+    private long getAttributeValueOffset(String targetAttrib) {
+      long idx = xmlReader.getLocationInfo().getStartingCharOffset();
+      idx += 1; // '<'
       String prefix = xmlReader.getPrefix();
       if (prefix != null && prefix.length() > 0) {
-        sb.append(prefix);
-        sb.append(':');
+        idx += prefix.length();
+        idx += 1; // ':'
       }
-      sb.append(xmlReader.getLocalName());
+      idx += xmlReader.getLocalName().length(); // 'tagname'
 
       // Any namespaces?
       int nsCount = xmlReader.getNamespaceCount();
       if (nsCount > 0) {
         for (int i = 0; i < nsCount; ++i) {
-          sb.append(" xmlns:");
-          sb.append(xmlReader.getNamespacePrefix(i));
-          sb.append("=\"");
-          sb.append(xmlReader.getNamespaceURI());
-          sb.append('"');
+          idx += 7; // ' xmlns:'
+          idx += xmlReader.getNamespacePrefix(i).length();
+          idx += 2; // '="'
+          idx += xmlReader.getNamespaceURI().length();
+          idx += 1; // '"'
         }
       }
-
-      int attrCount = xmlReader.getAttributeCount();
 
       // How about attrs?
+      int attrCount = xmlReader.getAttributeCount();
       if (attrCount > 0) {
         for (int i = 0;  i < attrCount; ++i) {
-          sb.append(' ');
-          sb.append(xmlReader.getAttributeName(i).toString());
-          sb.append("=\"");
-          sb.append(xmlReader.getAttributeValue(i));
-          sb.append('"');
+          idx += 1; // ' '
+          String attribName = xmlReader.getAttributeName(i).toString();
+          idx += attribName.length();
+          idx += 2; // '="'
+          if (targetAttrib.equals(attribName)) {
+            return idx;
+          }
+          idx += xmlReader.getAttributeValue(i).length();
+          idx += 1; // '"'
         }
       }
-
-      sb.append("/>");
-      return sb.toString();
+      return -1;
     }
   }
 }
