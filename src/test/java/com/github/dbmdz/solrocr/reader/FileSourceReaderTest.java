efc4523bd7cb8ddefa1df10dc7261a6b54416416
Johannes Baiter <johannes.baiter@bsb-muenchen.de>
2024-05-23 07:10:12 +0200

    Add missing tests for SourceReader implementations

---
+++ src/test/java/com/github/dbmdz/solrocr/reader/FileSourceReaderTest.java
@@ -0,0 +1,71 @@
+package com.github.dbmdz.solrocr.reader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.dbmdz.solrocr.model.SourcePointer;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.ByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class FileSourceReaderTest {
+
+  private final Path filePath = Paths.get("src/test/resources/data/hocr.html");
+  private final SourcePointer pointer = SourcePointer.parse(filePath.toString());
+  private final int maxCacheEntries = 10;
+
+  @Test
+  void shouldReadUtf8StringCorrectly() {
+    SourceReader reader = new FileSourceReader(filePath, pointer, 8192, maxCacheEntries);
+    // No UTF8 misalignment offset
+    assertThat(reader.readUtf8String(422871, 97))
+        .isEqualTo(
+            "<span class='ocrx_word' id='word_22_188' title='bbox 736 1671 865 1713; x_wconf 96'>dünne</span>");
+    // Misaligned start offset
+    assertThat(reader.readUtf8String(422957, 4)).isEqualTo("nne");
+    // Misaligned end offset
+    assertThat(reader.readUtf8String(422871, 86))
+        .isEqualTo(
+            "<span class='ocrx_word' id='word_22_188' title='bbox 736 1671 865 1713; x_wconf 96'>d");
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+      ints = {64, 128, 256, 512, 1024, 2048, 4096, 8192, 16_384, 32_768, 65_536, 131_072, 262_144})
+  void shouldReadAsciiStringCorrectlyWithDifferentSectionSizes(int sectionSize) throws IOException {
+    // Reduce number of cache entries to force some cache evictions
+    SourceReader reader = new FileSourceReader(filePath, pointer, sectionSize, 3);
+    // Choose offsets to force reading across multiple sections
+    int startOffset = 2_715_113 - (maxCacheEntries / 2 * sectionSize) - (sectionSize / 2);
+    int endOffset = startOffset + (maxCacheEntries / 2 * sectionSize);
+    int readLen = endOffset - startOffset;
+    byte[] expectedData = new byte[readLen];
+    try (ByteChannel chan = Files.newByteChannel(filePath).position(startOffset)) {
+      chan.read(ByteBuffer.wrap(expectedData));
+    }
+    String expectedStr = new String(expectedData, 0, 0, expectedData.length);
+    assertThat(reader.readAsciiString(startOffset, readLen)).isEqualTo(expectedStr);
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+      ints = {64, 128, 256, 512, 1024, 2048, 4096, 8192, 16_384, 32_768, 65_536, 131_072, 262_144})
+  public void shouldReadCorrectlyAlignedSections(int sectionSize) throws IOException {
+    SourceReader reader = new FileSourceReader(filePath, pointer, sectionSize, maxCacheEntries);
+    int offset = (sectionSize * 4) + (sectionSize / 2);
+    SourceReader.Section section = reader.getAsciiSection(offset);
+    byte[] expectedData = new byte[sectionSize];
+    try (ByteChannel chan = Files.newByteChannel(filePath).position(sectionSize * 4L)) {
+      chan.read(ByteBuffer.wrap(expectedData));
+    }
+    String expectedStr = new String(expectedData, 0, 0, expectedData.length);
+    assertThat(section.start).isEqualTo(sectionSize * 4);
+    assertThat(section.end).isEqualTo(sectionSize * 5);
+    assertThat(section.text).isEqualTo(expectedStr);
+  }
+}
---
+++ src/test/java/com/github/dbmdz/solrocr/reader/MultiFileSourceReaderTest.java
@@ -0,0 +1,119 @@
+package com.github.dbmdz.solrocr.reader;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import com.github.dbmdz.solrocr.model.SourcePointer;
+import com.github.dbmdz.solrocr.reader.SourceReader.Section;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+class MultiFileSourceReaderTest {
+  private final List<Path> filePaths;
+  private final SourcePointer pointer;
+  private final int maxCacheEntries = 10;
+
+  MultiFileSourceReaderTest() throws IOException {
+    Path root = Paths.get("src/test/resources/data/alto_multi");
+    filePaths = new ArrayList<>();
+    try (DirectoryStream<Path> stream = Files.newDirectoryStream(root, "1860-11-30*.xml")) {
+      stream.forEach(filePaths::add);
+    }
+    pointer =
+        SourcePointer.parse(
+            filePaths.stream()
+                .map(p -> p.toAbsolutePath().toString())
+                .collect(Collectors.joining("+")));
+  }
+
+  private byte[] readData(int startOffset, int endOffset) throws IOException {
+    if (startOffset >= endOffset) {
+      throw new IllegalArgumentException("startOffset must be less than endOffset");
+    }
+
+    byte[] data = new byte[endOffset - startOffset];
+    int outOffset = 0;
+    long offset = 0; // Use long to avoid overflow
+
+    for (Path filePath : filePaths) {
+      long fSize = Files.size(filePath);
+
+      if (offset + fSize <= startOffset) {
+        offset += fSize;
+        continue;
+      }
+
+      try (SeekableByteChannel chan = Files.newByteChannel(filePath)) {
+        long position = Math.max(0, startOffset - offset);
+        chan.position(position);
+        int bytesToRead = (int) Math.min(data.length - outOffset, fSize - position);
+
+        ByteBuffer buffer = ByteBuffer.wrap(data, outOffset, bytesToRead);
+        int bytesRead = 0;
+        while (buffer.hasRemaining()) {
+          int read = chan.read(buffer);
+          if (read == -1) {
+            break;
+          }
+          bytesRead += read;
+        }
+        outOffset += bytesRead;
+        offset += position + bytesRead;
+
+        if (offset >= endOffset) {
+          break;
+        }
+      }
+    }
+
+    if (outOffset < data.length) {
+      throw new IOException("Not enough data to read");
+    }
+
+    return data;
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+      ints = {64, 128, 256, 512, 1024, 2048, 4096, 8192, 16_384, 32_768, 65_536, 131_072, 262_144})
+  void shouldReadAsciiStringCorrectlyWithDifferentSectionSizes(int sectionSize) throws IOException {
+    // Reduce number of cache entries to force some cache evictions
+    SourceReader reader = new MultiFileSourceReader(filePaths, pointer, sectionSize, 3);
+    // Choose offsets to force reading across multiple sections
+    int startOffset =
+        Math.max(
+            0, (reader.length() / 2) - (maxCacheEntries / 2 * sectionSize) - (sectionSize / 2));
+    int endOffset = Math.min(startOffset + (maxCacheEntries / 2 * sectionSize), reader.length());
+    int readLen = endOffset - startOffset;
+    byte[] expectedData = readData(startOffset, endOffset);
+    String expectedStr = new String(expectedData, 0, 0, expectedData.length);
+    String actualStr = reader.readAsciiString(startOffset, readLen);
+    assertThat(actualStr).isEqualTo(expectedStr);
+  }
+
+  @ParameterizedTest
+  @ValueSource(
+      ints = {64, 128, 256, 512, 1024, 2048, 4096, 8192, 16_384, 32_768, 65_536, 131_072, 262_144})
+  public void shouldReadCorrectlyAlignedSections(int sectionSize) throws IOException {
+    SourceReader reader =
+        new MultiFileSourceReader(filePaths, pointer, sectionSize, maxCacheEntries);
+    // FIXME: Pick offset that falls on a file boundary!
+    int offset = (int) (Files.size(filePaths.get(2)) - 32);
+    Section section = reader.getAsciiSection(offset);
+    int sectionStart = (offset / sectionSize) * sectionSize;
+    byte[] expectedData = readData(sectionStart, sectionStart + sectionSize);
+    String expectedStr = new String(expectedData, 0, 0, expectedData.length);
+    assertThat(section.start).isEqualTo(sectionStart);
+    assertThat(section.end).isEqualTo(sectionStart + sectionSize);
+    assertThat(section.text).isEqualTo(expectedStr);
+  }
+}
