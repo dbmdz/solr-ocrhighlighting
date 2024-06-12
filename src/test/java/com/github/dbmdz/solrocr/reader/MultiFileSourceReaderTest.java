6f13216439eff7e61ecbc8585a36baf784a0b9f9
Katharina Schmid <katharina.schmid@bsb-muenchen.de>
2024-06-12 13:37:13 +0200

    Sort test input

--- src/test/java/com/github/dbmdz/solrocr/reader/MultiFileSourceReaderTest.java
+++ src/test/java/com/github/dbmdz/solrocr/reader/MultiFileSourceReaderTest.java
@@ -13,6 +13,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.apache.commons.io.IOUtils;
@@ -31,6 +32,7 @@ class MultiFileSourceReaderTest {
     try (DirectoryStream<Path> stream = Files.newDirectoryStream(root, "1860-11-30*.xml")) {
       stream.forEach(filePaths::add);
     }
+    filePaths.sort(Comparator.comparing(Path::toString));
     pointer =
         SourcePointer.parse(
             filePaths.stream()
